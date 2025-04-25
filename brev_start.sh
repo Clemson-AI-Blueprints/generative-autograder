df34c89c1e798206289146b5fe86339e7d65add4
Elan456 <telan4892@gmail.com>
2025-04-25 12:37:54 -0400

    brev_start sets env variables from .env file

--- brev_start.sh
+++ brev_start.sh
@@ -1,3 +1,13 @@
+#!/usr/bin/env bash
+set -e
+
+# 👇 Load .env variables into the current shell
+set -o allexport
+source .env
+set +o allexport
+
+echo "$NVIDIA_API_KEY" | docker login nvcr.io -u '$oauthtoken' --password-stdin
+# echo "$GITHUB_PAT" | docker login ghcr.io -u your-github-username --password-stdin
+
 docker compose -f deploy/compose/docker-compose-brev.yaml down
 docker compose -f deploy/compose/docker-compose-brev.yaml up -d --build
-
